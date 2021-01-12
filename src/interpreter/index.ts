5444a2a1989a426622f9491dc3d299f2d4893653
Tommy <tommycodebox@gmail.com>
2021-01-12 23:12:46 +0200

    Add jump commands and validation

--- src/interpreter/index.ts
+++ src/interpreter/index.ts
@@ -9,10 +9,15 @@ export const GT = 'GT'
 export const EQ = 'EQ'
 export const AND = 'AND'
 export const OR = 'OR'
+export const JUMP = 'JUMP'
+export const JUMPI = 'JUMPI'
 
+const EXECUTION_COMPLETE = 'Execution complete'
+const EXECUTION_LIMIT = 10000
 export class Interpreter {
   state: {
     programCounter: number
+    executionCount: number
     stack: any[]
     code: any[]
   }
@@ -20,9 +25,20 @@ export class Interpreter {
   constructor() {
     this.state = {
       programCounter: 0,
+      executionCount: 0,
       stack: [],
       code: [],
     }
+  }
+
+  jump() {
+    const destination = this.state.stack.pop()
+
+    if (destination < 0 || destination > this.state.code.length)
+      throw new Error(`Invalid destination: ${destination}`)
+
+    this.state.programCounter = destination
+    this.state.programCounter--
   }
 
   runCode(code: any) {
@@ -30,14 +46,25 @@ export class Interpreter {
     this.state.code = code
 
     while (this.state.programCounter < this.state.code.length) {
+      this.state.executionCount++
+
+      if (this.state.executionCount > EXECUTION_LIMIT)
+        throw new Error(
+          `Infinite loop detected, execution limit of ${EXECUTION_LIMIT} exceeded`,
+        )
+
       const opCode = this.state.code[this.state.programCounter]
 
       try {
         switch (opCode) {
           case STOP:
-            throw new Error('Execution complete')
+            throw new Error(EXECUTION_COMPLETE)
           case PUSH:
             this.state.programCounter++
+
+            if (this.state.programCounter === this.state.code.length)
+              throw new Error(`The PUSH instruction cannot be last`)
+
             const value = this.state.code[this.state.programCounter]
             this.state.stack.push(value)
             break
@@ -67,11 +94,22 @@ export class Interpreter {
 
             this.state.stack.push(result)
             break
+          case JUMP:
+            this.jump()
+            break
+          case JUMPI:
+            const condition = this.state.stack.pop()
+            if (condition === 1) this.jump()
+            break
           default:
             break
         }
       } catch (err) {
-        return this.state.stack[this.state.stack.length - 1]
+        if (err.message === EXECUTION_COMPLETE) {
+          return this.state.stack[this.state.stack.length - 1]
+        }
+
+        throw err
       }
 
       this.state.programCounter++
@@ -79,9 +117,67 @@ export class Interpreter {
   }
 }
 
-const interpreter = new Interpreter()
-let code, result
+// let code = [PUSH, 2, PUSH, 3, ADD, STOP]
+// let result = new Interpreter().runCode(code)
+// console.log('Result of 3 ADD 2:', result)
 
-code = [PUSH, 1, PUSH, 0, OR, STOP]
-result = interpreter.runCode(code)
-console.log({ result })
+// code = [PUSH, 2, PUSH, 3, SUB, STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of 3 SUB 2:', result)
+
+// code = [PUSH, 2, PUSH, 3, MUL, STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of 3 MUL 2:', result)
+
+// code = [PUSH, 2, PUSH, 3, DIV, STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of 3 DIV 2:', result)
+
+// code = [PUSH, 2, PUSH, 3, LT, STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of 3 LT 2:', result)
+
+// code = [PUSH, 2, PUSH, 3, GT, STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of 3 GT 2:', result)
+
+// code = [PUSH, 2, PUSH, 2, EQ, STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of 2 EQ 2:', result)
+
+// code = [PUSH, 1, PUSH, 0, AND, STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of 0 AND 1:', result)
+
+// code = [PUSH, 1, PUSH, 0, OR, STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of 0 OR 1:', result)
+
+// code = [PUSH, 6, JUMP, PUSH, 0, JUMP, PUSH, 'jump successful', STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of JUMP:', result)
+
+// code = [PUSH, 8, PUSH, 1, JUMPI, PUSH, 0, JUMP, PUSH, 'jump successful', STOP]
+// result = new Interpreter().runCode(code)
+// console.log('Result of JUMPI:', result)
+
+// code = [PUSH, 99, JUMP, PUSH, 0, JUMP, PUSH, 'jump successful', STOP]
+// try {
+//   new Interpreter().runCode(code)
+// } catch (error) {
+//   console.log('Invalid destination error:', error.message)
+// }
+
+// code = [PUSH, 0, PUSH]
+// try {
+//   new Interpreter().runCode(code)
+// } catch (error) {
+//   console.log('Expected invalid PUSH error:', error.message)
+// }
+
+// code = [PUSH, 0, JUMP, STOP]
+// try {
+//   new Interpreter().runCode(code)
+// } catch (error) {
+//   console.log('Expected invalid execution error:', error.message)
+// }
