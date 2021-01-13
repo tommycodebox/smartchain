1a6c540cccbf9cda522841e57986f3b8a7b8516b
Tommy <tommycodebox@gmail.com>
2021-01-13 09:33:16 +0200

    Add method to blockchain to add blocks

--- src/blockchain/index.ts
+++ src/blockchain/index.ts
@@ -2,9 +2,17 @@ export * from './block'
 
 import { Block } from './block'
 
+interface AddBlockProps {
+  block: Block
+}
+
 export class Blockchain {
   chain: Block[]
   constructor() {
     this.chain = [Block.genesis()]
+  }
+
+  add({ block }: AddBlockProps) {
+    this.chain.push(block)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,20 @@ const blockchain = new Blockchain()
 // console.log(JSON.stringify(blockchain, null, 2))
 
 // console.log(sortChars(blockchain))
-console.log(
-  Block.mine({
-    lastBlock: Block.genesis(),
-    beneficiary: 'fooo',
-  }),
-)
+// console.log(
+//   Block.mine({
+//     lastBlock: Block.genesis(),
+//     beneficiary: 'fooo',
+//   }),
+// )
+
+// for (let i = 0; i < 1000; i++) {
+//   const lastBlock = blockchain.chain[blockchain.chain.length - 1]
+//   const block = Block.mine({
+//     lastBlock,
+//     beneficiary: 'beneficiary',
+//   })
+
+//   blockchain.add({ block })
+//   console.log(block)
+// }
