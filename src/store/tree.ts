f2bd85bccdeb85ca5da703d0956e42a210132686
Tommy <tommycodebox@gmail.com>
2021-01-15 09:33:34 +0200

    Add tree for state management

---
+++ src/store/tree.ts
@@ -0,0 +1,57 @@
+import { keccakHash } from '@/util'
+
+interface PutProps {
+  key: string
+  value: any
+}
+
+class Node {
+  value: any
+  childMap: Record<string, Node>
+
+  constructor() {
+    this.value = null
+    this.childMap = {}
+  }
+}
+
+export class Tree {
+  head: Node
+  rootHash: string
+  constructor() {
+    this.head = new Node()
+    this.generateRootHash()
+  }
+
+  generateRootHash() {
+    this.rootHash = keccakHash(this.head)
+  }
+
+  get(key: string) {
+    let node = this.head
+
+    for (let char of key) {
+      if (node.childMap[char]) {
+        node = node.childMap[char]
+      }
+    }
+
+    return node.value
+  }
+
+  put({ key, value }: PutProps) {
+    let node = this.head
+
+    for (let char of key) {
+      if (!node.childMap[char]) {
+        node.childMap[char] = new Node()
+      }
+
+      node = node.childMap[char]
+    }
+
+    node.value = value
+
+    this.generateRootHash()
+  }
+}
